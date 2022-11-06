ad4c9503a7a59c23d810d9d9a543443fd593d603
Robert Grabiński <bob.grabinski@gmail.com>
2022-11-06 11:44:16 +0100

    Add Game Service layer

--- src/main/java/games/driftingpixel/gamestore/controllers/ControllerGame.java
+++ src/main/java/games/driftingpixel/gamestore/controllers/ControllerGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 import games.driftingpixel.gamestore.models.db.Game;
 import games.driftingpixel.gamestore.models.exceptions.WrongIdException;
 import games.driftingpixel.gamestore.repositories.RepositoryGame;
+import games.driftingpixel.gamestore.services.ServiceGame;
 import games.driftingpixel.gamestore.utility.comparators.ComparatorSortByIdDesc;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,27 +30,14 @@ public class ControllerGame {
 
   private static final String ENDPOINT_PREFIX = "/games";
 
-  private final RepositoryGame repositoryGame;
+  private final ServiceGame serviceGame;
 
-  //Par for all users
+  //Part for all users
 
   @GetMapping(ControllerGame.ENDPOINT_PREFIX + "/{id}")
-  Game one(@PathVariable Long id) {
+  ResponseEntity<Game> one(@PathVariable Long id) {
     log.info("Endpoint -> Game by id: {}", id);
-    return repositoryGame.findById(id).orElseThrow(() -> new WrongIdException());
-  }
-
-  // Part for registered users / admins
-
-  /**
-   * @return all games sorted by id desc
-   */
-  @GetMapping(ControllerGame.ENDPOINT_PREFIX)
-  List<Game> all() {
-    log.info("Endpoint -> Get all games");
-    List<Game> result = repositoryGame.findAll();
-    result.sort(new ComparatorSortByIdDesc());
-    return result;
+    return  ResponseEntity.ok(serviceGame.getGameById(id));
   }
 
   /**
@@ -60,36 +48,43 @@ public class ControllerGame {
   @GetMapping(params = { "page", "size" }, path=ControllerGame.ENDPOINT_PREFIX )
   ResponseEntity<Page<Game>> getPageable(@SortDefault(sort = "id") @PageableDefault(size = 20) final Pageable pageable) {
     log.info("Endpoint -> Games by page: {}", pageable);
-    Page<Game> result = repositoryGame.findAll(pageable);
+    Page<Game> result = serviceGame.getGamesPageable(pageable);
    
     return ResponseEntity.ok(result);
   }
 
+  // Part for registered users / admins
+
+  /**
+   * @return all games sorted by id desc
+   */
+  @GetMapping(ControllerGame.ENDPOINT_PREFIX)
+  ResponseEntity<List<Game>> all() {
+    log.info("Endpoint -> Get all games");
+    List<Game> result = serviceGame.getAllGames();
+    result.sort(new ComparatorSortByIdDesc());
+    return  ResponseEntity.ok(result);
+  }
+
+  
+
 
   @PostMapping(ControllerGame.ENDPOINT_PREFIX)
-  Game newElement(@RequestBody Game newGame) {
+  ResponseEntity<Game> newGame(@RequestBody Game newGame) {
     log.info("Endpoint -> Add new game: {}", newGame);
-    return repositoryGame.save(newGame);
+    return  ResponseEntity.ok(serviceGame.addNewGame(newGame));
   }
 
   @PutMapping(ControllerGame.ENDPOINT_PREFIX + "/{id}")
-  Game replaceElement(@RequestBody Game newGame, @PathVariable Long id) {
+  ResponseEntity<Game> replaceElement(@RequestBody Game newGame, @PathVariable Long id) {
     log.info("Endpoint -> Update game: id {}, newGame {}", id, newGame);
-    return repositoryGame.findById(id)
-        .map((game) -> { 
-          game.copyFromGame(newGame, false);
-          return repositoryGame.save(game);
-        })
-        .orElseGet(() -> {
-          newGame.setId(id);
-          return repositoryGame.save(newGame);
-        });
+    return  ResponseEntity.ok(serviceGame.updateGame(newGame, id));
   }
 
   @DeleteMapping(ControllerGame.ENDPOINT_PREFIX + "/{id}")
-  void deleteEmployee(@PathVariable Long id) {
+  void delete(@PathVariable Long id) {
     log.info("Endpoint -> Delete game by id: {}", id);
-    repositoryGame.deleteById(id);
+    serviceGame.deleteGame(id);
   }
   
 }
---
+++ src/main/java/games/driftingpixel/gamestore/services/ServiceGame.java
@@ -0,0 +1,94 @@
+package games.driftingpixel.gamestore.services;
+
+import java.util.List;
+
+import games.driftingpixel.gamestore.models.db.Game;
+import games.driftingpixel.gamestore.models.exceptions.WrongIdException;
+import games.driftingpixel.gamestore.repositories.RepositoryGame;
+import games.driftingpixel.gamestore.utility.comparators.ComparatorSortByIdDesc;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class ServiceGame {
+
+  private RepositoryGame repositoryGame;
+
+  /***
+   * 
+   * @param id
+   * @return Game from repository with selected id 
+   */
+  public Game getGameById( Long id) throws WrongIdException {
+    return repositoryGame.findById(id).orElseThrow(() -> new WrongIdException());
+  }
+
+
+  /***
+   * 
+   * @return all games sorted by id desc
+   */
+  public List<Game> getAllGames() {
+    List<Game> result = repositoryGame.findAll();
+    log.debug("All Game list from repository has {} games", result.size());
+    log.trace("All Game list from repository: {}", result);
+    result.sort(new ComparatorSortByIdDesc());
+    return result;
+  }
+
+  /**
+   * @param pageable
+   * @return all games with pagination
+   */
+  public Page<Game> getGamesPageable(Pageable pageable) {
+    Page<Game> result = repositoryGame.findAll(pageable);
+    log.debug("Page of games as {} games", result.getNumberOfElements());
+    log.trace("Game page from repository: {} for pageable {}", result, pageable);
+    return result;
+  }
+
+  /***
+   * Add new game to db
+   * @param newGame
+   * @return
+   */
+  public Game addNewGame(Game newGame) {
+    log.debug("Add new game: {}", newGame);
+    return repositoryGame.save(newGame);
+  }
+
+  /***
+   * Update select game by new game data
+   * @param newGame
+   * @param id
+   * @return
+   */
+  public Game updateGame(Game newGame, Long id) {
+    log.debug("update game: id {}, newGame {}", id, newGame);
+    return repositoryGame.findById(id)
+        .map((game) -> { 
+          log.debug("Game before update {}", game);
+          game.copyFromGame(newGame, false);
+          return repositoryGame.save(game);
+        })
+        .orElseGet(() -> {
+          newGame.setId(id);
+          return repositoryGame.save(newGame);
+        });
+  }
+
+  /***
+   * Dalete game from db
+   * @param id
+   */
+  public void deleteGame(Long id) {
+    log.debug("Remove game by id: {}", id);
+    repositoryGame.deleteById(id);
+  }
+  
+}
